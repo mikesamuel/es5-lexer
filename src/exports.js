c6872541de649da97debcaec794777c768fc0f4a
Mike Samuel <mikesamuel@gmail.com>
2011-07-10 10:48:05 -0700

    Package public symbols into a map to avoid namespace pollution in the compiled bundle

---
+++ src/exports.js
@@ -0,0 +1,7 @@
+global["es5Lexer"] = {
+  "makeScanner": makeScanner,
+  "classifyToken": classifyToken,
+  "isLineTerminator": isLineTerminator,
+  "disambiguateTokenStream": disambiguateTokenStream,
+  "TokenType": TokenType
+};
